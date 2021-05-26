6d6f3f7fdd123b7f8e7743a3fce4cf15cabb7496
VictorJorgeFGA <victor.eng.unb@gmail.com>
2021-05-26 12:44:15 -0300

    Add before action security to staff area

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,4 +1,33 @@
 class BlogController < ApplicationController
+  before_action :authenticate_user!, :require_admin_role!, only: [:staff_area]
+
   def news
+  end
+
+  def game_updates
+  end
+
+  def champions
+  end
+
+  def items
+  end
+
+  def routes
+  end
+
+  def game_modes
+  end
+
+  def policy
+  end
+
+  def staff_area
+  end
+
+  private def require_admin_role!
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 end
